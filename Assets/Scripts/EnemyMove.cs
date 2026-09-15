887be7eaaf75136f59a0975b86001e6901791119
SamuelAugust0 <>
2026-09-15 19:52:20 -0300

    q

--- Assets/Scripts/EnemyMove.cs
+++ Assets/Scripts/EnemyMove.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class EnemyMove : MonoBehaviour
 {
+    
+  a
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
