6c47046b58a9ab97f4266fbc173008855486d233
Dudu - PC Senai <anonimoaleatorio465@gmail.com>
2026-09-22 21:45:53 -0300

    Player Move - Criei o movimento, xp, level e as variaveis de velocidade, vida, pontuação e tiro.

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -1,7 +1,18 @@
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
-{
+
+{   int vida = 100;
+    float velocidade = 5.0f;
+
+    int pontuacao = 0;
+
+    string tipoDeTiro = "Bala Simples";
+
+    int xp = 0;
+
+    int level = 1;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +22,42 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Movimento();
     }
+
+    void GanharXp (int xpGanho)
+    {
+        xp = xp + xpGanho;
+
+        if (xp >= 100)
+        {
+            level = level + 1;
+            xp = xp - 100;
+        }
+
+    }
+
+    void Movimento()
+        {
+            if (Input.GetKey(KeyCode.W))
+        {
+            transform.position = transform.position + new Vector3 (0, 1, 0) * velocidade * Time.deltaTime;
+
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position = transform.position + new Vector3 (-1, 0, 0) * velocidade * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position = transform.position + new Vector3 (0, -1, 0) * velocidade * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position = transform.position + new Vector3 (1, 0, 0) * velocidade * Time.deltaTime;
+        }
+        }
 }
